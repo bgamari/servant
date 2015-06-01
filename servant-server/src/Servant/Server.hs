faead8c6fdb5809221cddd2919ea059955d9c360
Andres Loeh <andres@well-typed.com>
2015-06-01 19:38:51 +0200

    Refactoring: one module per concept.
    The main `Server.Internal` module was getting a bit large for my taste.
    It now contains just the instances. All the administrative utilities
    are in their own dedicated modules.

--- servant-server/src/Servant/Server.hs
+++ servant-server/src/Servant/Server.hs
@@ -81,6 +81,8 @@ import           Data.Proxy                         (Proxy)
 import           Network.Wai                        (Application)
 import           Servant.Server.Internal
 import           Servant.Server.Internal.Enter
+import           Servant.Server.Internal.Router
+import           Servant.Server.Internal.RoutingApplication
 import           Servant.Server.Internal.ServantErr
 
 
